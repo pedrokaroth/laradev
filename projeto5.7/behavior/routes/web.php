7bad1c50a63fc611bcc41ba122b27eb4512e9ed0
pedrokaroth <pedrokaroth@gmail.com>
2021-02-01 09:21:23 -0300

    Definindo Rota

--- projeto5.7/behavior/routes/web.php
+++ projeto5.7/behavior/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/imoveis', 'PropertyController@index');
+
+Route::get('/imoveis/criar', 'PropertyController@create');
