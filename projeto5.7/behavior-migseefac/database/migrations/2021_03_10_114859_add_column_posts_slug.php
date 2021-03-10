e5441d36069003593fbbb50ef92d817512b4abca
pedrokaroth <pedrokaroth@gmail.com>
2021-03-10 08:51:57 -0300

    [SCHEMA] Modelando uma tabela completa

--- projeto5.7/behavior-migseefac/database/migrations/2021_03_10_114859_add_column_posts_slug.php
+++ projeto5.7/behavior-migseefac/database/migrations/2021_03_10_114859_add_column_posts_slug.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class AddColumnPostsSlug extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->string('subtitle');
-            $table->text('content');
-            $table->dateTime('publish_at');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('slug')->unique()->after('title');
         });
     }
 
@@ -30,6 +25,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
     }
 }
