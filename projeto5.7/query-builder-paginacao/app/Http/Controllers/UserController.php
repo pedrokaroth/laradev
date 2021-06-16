7973b194ed760a4829df4172f1aca9955b4371e0
pedrokaroth <pedrokaroth@gmail.com>
2021-06-16 19:03:22 -0300

    Construtor de Query

--- projeto5.7/query-builder-paginacao/app/Http/Controllers/UserController.php
+++ projeto5.7/query-builder-paginacao/app/Http/Controllers/UserController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = DB::table('users')
+            ->select('users.id', 'users.name', 'users.status')
+            ->where('users.status', '=', '1')
+            ->orderBy('name')
+            //->oldest('name') // ASC order
+            //->latest('name') // DESC order
+            //->inRandomOrder() // RAND order
+            ->limit(10)
+            ->offset(10)
+            ->get();
+        foreach ($users as $user) {
+            echo "#{$user->id} | {$user->name} | {$user->status}<br>";
+        }
+
+        var_dump($users);
+    }
 }
