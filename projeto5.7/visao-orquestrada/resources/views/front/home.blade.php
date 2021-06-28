54e6d741304da0b49151d964785458b62fdfb77f
pedrokaroth <pedrokaroth@gmail.com>
2021-06-28 20:11:43 -0300

    Compondo Página Master

--- projeto5.7/visao-orquestrada/resources/views/front/home.blade.php
+++ projeto5.7/visao-orquestrada/resources/views/front/home.blade.php
@@ -1,40 +1,46 @@
-Meu nome é {{ $user->name }}
+@extends('front.master.master')
+@section('title', 'Seja muito bem vindo')
+@section('content')
 
-{!! $alert !!}
+    Meu nome é {{ $user->name }}
 
-@if(!empty($user->email))
-    <p>O usuário informou um email</p>
-@elseif($user)
-    <p>Existe um objeto usuário</p>
-@else
-    <p>Não existe um objeto usuário</p>
-@endif
+    {!! $alert !!}
 
-@isset($user)
-    <p>Existe um usuário setado</p>
-@endisset
+    @if(!empty($user->email))
+        <p>O usuário informou um email</p>
+    @elseif($user)
+        <p>Existe um objeto usuário</p>
+    @else
+        <p>Não existe um objeto usuário</p>
+    @endif
 
-@empty($user)
-    <p>Usuario vazio</p>
-@endempty
+    @isset($user)
+        <p>Existe um usuário setado</p>
+    @endisset
 
-@switch($user->level)
-    @case(1)
-    <p>Nivel 1</p>
-    @break
+    @empty($user)
+        <p>Usuario vazio</p>
+    @endempty
 
-    @case(2)
-    <p>Nivel 2</p>
-    @break
-@endswitch
+    @switch($user->level)
+        @case(1)
+        <p>Nivel 1</p>
+        @break
 
-<h2>Listagem de Cursos</h2>
-@foreach($courses as $course)
-    <div style="border: 1px solid black">
-        <h3>{{ $course['name'] }}</h3>
-        <p>Tutor: {{ $course['tutor'] }}</p>
-        @php
-            var_dump($loop);
-        @endphp
-    </div>
-@endforeach
+        @case(2)
+        <p>Nivel 2</p>
+        @break
+    @endswitch
+
+    <h2>Listagem de Cursos</h2>
+    @foreach($courses as $course)
+        <div style="border: 1px solid black">
+            <h3>{{ $course['name'] }}</h3>
+            <p>Tutor: {{ $course['tutor'] }}</p>
+            @php
+                var_dump($loop);
+            @endphp
+        </div>
+    @endforeach
+
+@endsection
