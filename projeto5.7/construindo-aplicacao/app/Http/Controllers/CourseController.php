2a0110a668f9e4ac4af93e70bb3387e70186d494
pedrokaroth <pedrokaroth@gmail.com>
2021-07-01 19:05:09 -0300

    Validando com Request

--- projeto5.7/construindo-aplicacao/app/Http/Controllers/CourseController.php
+++ projeto5.7/construindo-aplicacao/app/Http/Controllers/CourseController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CourseController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
@@ -19,7 +20,7 @@ class CourseController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -29,19 +30,32 @@ class CourseController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
-        var_dump($request->all());
+        $rules = [
+            'course' => 'required',
+            'tutor' => 'required|min:3|max:8',
+            'email' => 'required|email'
+        ];
+
+        $messages = [
+            'course.required' => 'O nome do curso deve ser preenchido.',
+            'tutor.required' => 'O nome do tutor deve ser preenchido.',
+            'email.required' => 'O email deve ser preenchido.',
+            'email.email' => 'Por favor informe um email valido'
+        ];
+
+        $request->validate($rules, $messages);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show($id)
     {
@@ -52,7 +66,7 @@ class CourseController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit($id)
     {
@@ -62,9 +76,9 @@ class CourseController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -75,7 +89,7 @@ class CourseController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
--- projeto5.7/construindo-aplicacao/resources/views/template/form.blade.php
+++ projeto5.7/construindo-aplicacao/resources/views/template/form.blade.php
@@ -5,21 +5,42 @@
     <div class="container">
         <div class="row">
             <div class="col-12 mt-4">
+                -
+                @foreach($errors->all() as $error)
+                    <div class="alert alert-danger" role="alert">
+                        {{ $error }}
+                    </div>
+                @endforeach
 
                 <form action="{{ route('course.store') }}" method="post" autocomplete="off" >
                     @csrf
 
                     <div class="form-group">
                         <label for="course">Curso:</label>
-                        <input type="text" id="course" name="course" class="form-control" placeholder="Insira o nome do curso">
+                        <input type="text" id="course" name="course" class="form-control {{ ($errors->has('course') ? 'is-invalid' : '') }}" placeholder="Insira o nome do curso" value="{{old('course')}}">
+                        @if($errors->has('course'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('course') }}
+                            </div>
+                        @endif
                     </div>
                     <div class="form-group">
                         <label for="tutor">Tutor:</label>
-                        <input type="text" id="tutor" name="tutor" class="form-control" placeholder="Insira o nome do tutor do curso">
+                        <input type="text" id="tutor" name="tutor" class="form-control  {{ ($errors->has('tutor') ? 'is-invalid' : '') }}" placeholder="Insira o nome do tutor do curso" value="{{old('tutor')}}">
+                        @if($errors->has('tutor'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('tutor') }}
+                            </div>
+                        @endif
                     </div>
                     <div class="form-group">
                         <label for="email">Email:</label>
-                        <input type="text" id="email" name="email" class="form-control" placeholder="Insira o email do tutor">
+                        <input type="text" id="email" name="email" class="form-control  {{ ($errors->has('email') ? 'is-invalid' : '') }}" placeholder="Insira o email do tutor" value="{{old('email')}}">
+                        @if($errors->has('email'))
+                            <div class="invalid-feedback">
+                                {{ $errors->first('email') }}
+                            </div>
+                        @endif
                     </div>
 
                     <div class="form-group">
